724fb4b7854b07bcecce726be7dfcee0c282c2d1
Nischal <kaflenk123@gmail.com>
2022-08-21 07:19:32 +0545

    Day 6 is added

---
+++ Day6/oop.py
@@ -0,0 +1,11 @@
+class Nischal:
+    print("INside class")
+
+    def __init__(self):
+        print("hello hello this is dada tangs ")
+
+
+o1 = Nischal()
+o2=Nischal()
+o3=Nischal()
+print(type(o1))
